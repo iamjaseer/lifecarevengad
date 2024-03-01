55f9b408c93c32213545c6afa130aa7342c95675
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-03-01 17:32:51 +0530

    Add files via upload

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -25,7 +25,7 @@ export default function Slider(props)  {
             <Link aria-label="Doctor" href={'doctors#' + doctor.doctorACF.id}>
               <Image width={doctor.featuredImage.node.mediaDetails.width} height={doctor.featuredImage.node.mediaDetails.height} src={doctor.featuredImage.node.sourceUrl} className='d-block w-100 doctor-photo mx-auto' alt={doctor.featuredImage.node.altText} />
               <div className="text-center pt-4">
-                <h4 className='h6 text-tertiary text-capitalize'>{doctor.title}</h4>
+                <h3 className='h6 text-tertiary text-capitalize'>{doctor.title}</h3>
                 <p className='p text-tertiary text-capitalize'>{doctor.doctorACF.id.replace(/_/g, " ")}</p>
               </div>
             </Link>
