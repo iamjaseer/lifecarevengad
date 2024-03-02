cd54911f874b35f4a27f3bc91e056ed9dad676b2
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-03-02 15:52:47 +0530

    Add files via upload

--- src/app/components/ContactInfo.jsx
+++ src/app/components/ContactInfo.jsx
@@ -20,7 +20,7 @@ const contactInfos = pagedata.data.allContactInfos.edges[0].node
                 <div className="row">
                     <div className="col-12">
                         <div className="box p-sm-5 d-lg-flex align-items-center justify-content-between" >
-                            <p className="me-lg-5">{contactInfos.contactInfoAcf.apponmentPageTopBoxDescription}</p>
+                            <h1 className="me-lg-5 text-normal">{contactInfos.contactInfoAcf.apponmentPageTopBoxDescription}</h1>
                             <Link aria-label="Call Us" href={"tel:" + contactInfos.contactInfoAcf.phone} class="btn btn-secondary btn-sm-full py-3 px-3 text-uppercase mt-4 mt-lg-0">Call us {contactInfos.contactInfoAcf.phone}</Link>
                         </div>
                     </div>
