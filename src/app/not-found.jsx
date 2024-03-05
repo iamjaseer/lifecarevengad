4544b1f67a416fba5b5a95461ce6b9a446bceaa7
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-03-05 10:29:26 +0530

    Add files via upload

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,20 +3,18 @@ import Link from "next/link";
 
 export default function PageNotFound() {
   return (<>
-    <div style={{ marginTop: '70px' }}>
       <section className="spacing-150 text-center">
         <div className="container">
           <div className="row">
             <div className="col-12 d-flex align-items-center justify-content-center">
               <div className="text-center">
-                <h1 className="text-primary"  style={{ fontSize: '10vw' }}>404</h1>
-                <p  className="text-uppercase heading-tertiary text-tertiary">WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND</p>
+                <h1 className="text-primary"  style={{ fontSize: '10vw', lineHeight: '0.8' }}>404</h1>
+                <p  className="text-uppercase font-weight-600 text-tertiary mt-5">WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND</p>
                 <Link aria-label="Go home" className="btn btn-primary py-3 px-4 mt-sm-4 mt-1" href={'/'}>Go home</Link>
               </div>
             </div>
           </div>
         </div>
       </section>
-    </div>
   </>)
 }
